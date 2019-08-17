31a9cf237dded5ba9e30ba97f1749942fe231aa6
Carlo Cadiz <carlocadiz71@gmail.com>
2019-08-16 22:50:08 -0400

    added hooks to ResoureceList component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,34 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = {
-    resources: [],
+const useResources = (resourceName) => {
+  const [ resources, setResources ] = useState([]);
+
+  const fetchResources = async(resourceName) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resourceName}`);
+    setResources(response.data);
   }
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+  useEffect(() => {
+    fetchResources(resourceName)
+  }, [resourceName]);
 
-    this.setState({ resources: response.data });
-  }
+  return resources;
+}
 
-  async componentDidUpdate(prevProps) {
-    if(prevProps.resourceName !== this.props.resourceName) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+// class ResourceList extends React.Component {
+//   state = {
+//     resources: [],
+//   }
 
-      this.setState({ resources: response.data });
-    }
-  }
+//   async componentDidMount() {
+//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
 
-  render() {
+//     this.setState({ resources: response.data });
+//   }
+
+//   async componentDidUpdate(prevProps) {
+//     if(prevProps.resourceName !== this.props.resourceName) {
+//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+
+//       this.setState({ resources: response.data });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {this.state.resources.map(resource => (
+//           <li key={resource.id}>{resource.title}</li>
+//         ))}
+//       </ul>
+//     )
+//   }
+// }
+
+const ResourceList = ({resourceName}) => {
+
+  const resources = useResources(resourceName);
+
     return (
       <ul>
-        {this.state.resources.map(resource => (
-          <li key={resource.id}>{resource.title}</li>
+        {resources.map(resource => (
+         <li key={resource.id}>{resource.title}</li>
         ))}
       </ul>
     )
-  }
+     
 }
 
 export default ResourceList;
